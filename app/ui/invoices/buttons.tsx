cda65fd5ea57dcd8e5d51cd55a217052950f2bc0
Peter Shen <pshen4@gmail.com>
2024-01-01 20:59:42 -0700

    remove mistake

--- app/ui/invoices/buttons.tsx
+++ app/ui/invoices/buttons.tsx
@@ -1,4 +1,4 @@
-import { deleteInvoice, deleteInvoiceWithId } from '@/app/lib/actions';
+import { deleteInvoice } from '@/app/lib/actions';
 import { PencilIcon, PlusIcon, TrashIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 
@@ -27,7 +27,7 @@ export function UpdateInvoice({ id }: { id: string }) {
 
 export function DeleteInvoice({ id }: { id: string }) {
   const deleteInvoiceWithId = deleteInvoice.bind(null, id);
-  
+
   return (
     <form action={deleteInvoiceWithId}>
       <button className="rounded-md border p-2 hover:bg-gray-100">
